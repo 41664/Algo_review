37857df48becaa1f92d6834a67d1a73f12d9d785
Ankit <jha0112358@gmail.com>
2018-12-11 02:25:26 +0530

    Added Pattern program

--- Pattern Printing/4x4 stars.cpp
+++ Pattern Printing/4x4 stars.cpp
@@ -1,5 +1,6 @@
 //this program witll print 4 rows and 4 colus of stars
 #include<bits/stdc++.h>
+using namespace std;
 int main()
 {
     int x=4;
@@ -7,8 +8,13 @@ int main()
     {
         for(int j=1;j<=x;j++)
         {
-          cout<<"X"
+          cout<<"X";
         }
+        cout<<"\n";
     }
     return 0;
 }
+/* XXXX
+XXXX
+XXXX
+XXXX */
